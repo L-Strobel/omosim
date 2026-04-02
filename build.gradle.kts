d2208f6940975f3c714cba518bc6aee3b32c145b
les29rq <leo.strobel@uni-wuerzburg.de>
2026-04-02 12:09:09 +0200

    Calibration: GradientDescent, Backtracking Line Search

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,7 @@ allOpen {
 }
 
 group = "de.uniwuerzburg.omosim"
-version = "2.3.2-DFM-6"
+version = "2.3.2-DFM-7"
 
 repositories {
     mavenLocal()
--- src/main/kotlin/de/uniwuerzburg/omosim/calibration/algorithms/GradientDescent.kt
+++ src/main/kotlin/de/uniwuerzburg/omosim/calibration/algorithms/GradientDescent.kt
@@ -3,7 +3,9 @@ package de.uniwuerzburg.omosim.calibration.algorithms
 import de.uniwuerzburg.omosim.calibration.differentiablemodel.DifferentiableModelSingleOut
 import org.jetbrains.kotlinx.multik.api.stat.abs
 import kotlin.math.abs
+import kotlin.math.pow
 import kotlin.time.measureTime
+import kotlin.time.measureTimedValue
 
 object GradientDescent {
     private const val NAME = "GradientDescent"
@@ -14,6 +16,7 @@ object GradientDescent {
         const val lb = 1e-3
         const val ub = 1e3
         val lTol: Double? = null
+        val backTracking: Boolean = false
     }
 
     fun run(
@@ -28,7 +31,8 @@ object GradientDescent {
             lr0 = parameters?.get("lr0")?.toDoubleOrNull() ?: Defaults.lr0,
             lb = parameters?.get("lb")?.toDoubleOrNull() ?: Defaults.lb,
             ub = parameters?.get("ub")?.toDoubleOrNull() ?: Defaults.ub,
-            lTol = parameters?.get("lTol")?.toDoubleOrNull() ?: Defaults.lTol
+            lTol = parameters?.get("lTol")?.toDoubleOrNull() ?: Defaults.lTol,
+            backTracking = parameters?.get("backTracking")?.toBoolean() ?: Defaults.backTracking,
         )
     }
 
@@ -39,38 +43,57 @@ object GradientDescent {
         lr0: Double = Defaults.lr0,
         lb: Double = Defaults.lb,
         ub: Double = Defaults.ub,
-        lTol: Double? = Defaults.lTol
+        lTol: Double? = Defaults.lTol,
+        backTracking: Boolean = Defaults.backTracking
     ) : DoubleArray {
         ProgressLogger.logParameters(this.NAME,"lr0=$lr0:lb=$lb:ub$ub")
 
         // Init
-        val lr = lr0
-        val x = x0.copyOf()
+        var x = x0.copyOf()
         var bestX = x0.copyOf()
         var bestLoss = model.evaluate(x0)
         var lastLoss = bestLoss
         var nNoImprovement = 0
+        var btMinStepSizeReached = false
         ProgressLogger.logInitialLoss(this.NAME, bestLoss)
 
         // Descent
         ProgressLogger.logProgressHeader()
         for (i in 0 until iterations) {
             val g = DoubleArray(x0.size) { 0.0 }
-            val time = measureTime {
+            val (loss, time) = measureTimedValue {
                 // Compute Gradient
                 model.gradientReverse(x, g, 1.0)
 
-                // Step
-                for (j in x.indices) {
-                    x[j] -= lr * g[j]
+                // Evaluate
+                var newX = updateX(x, g, lr0, lb, ub) // Gradient Step and Bound Handling
+                var loss = model.evaluate(newX)
+
+                // Backtracking
+                if (backTracking) {
+                    val tau = 0.1 // Learning rate shrinkage
+                    val c = 1e-4  // Acceptable achieved fraction of linear improvement promised by the gradient
+                    val minLr = 1e-10
+                    val t = c * g.sumOf { it.pow(2) }
+
+                    var lr = lr0
+                    while(lastLoss - loss < lr*t) { // Armijo Condition
+                        lr *= tau
+                        newX = updateX(x, g, lr, lb, ub)
+                        loss = model.evaluate(newX)
+
+                        if (lr <= minLr) {
+                            btMinStepSizeReached = true
+                            break
+                        }
+                    }
                 }
 
-                // Bound Projection
-                x.project(lb, ub)
+                x = newX
+                loss
             }
 
-            // Evaluate
-            val loss = model.evaluate(x)
+            // Update best solution
             if (loss < bestLoss) {
                 bestX = x.copyOf()
                 bestLoss = loss
@@ -94,6 +117,12 @@ object GradientDescent {
                 )
                 break
             }
+            if (btMinStepSizeReached) {
+                ProgressLogger.logEarlyTermination(
+                    this.NAME, reason = "Reached minimum step size during backtracking."
+                )
+                break
+            }
             if (nNoImprovement > 5) {
                 ProgressLogger.logEarlyTermination(
                     this.NAME, reason = "No improvement in 5 iterations"
@@ -112,6 +141,20 @@ object GradientDescent {
         }
         ProgressLogger.logFinalLoss(this.NAME, bestLoss)
         return bestX
+    }
+
+    fun updateX(x: DoubleArray, g: DoubleArray, lr: Double, lb: Double, ub: Double) : DoubleArray {
+        val xCopy = x.copyOf()
+
+        // Step
+        for (j in x.indices) {
+            xCopy[j] -= lr * g[j]
+        }
+
+        // Bound Projection
+        xCopy.project(lb, ub)
+
+        return xCopy
     }
 }
 
