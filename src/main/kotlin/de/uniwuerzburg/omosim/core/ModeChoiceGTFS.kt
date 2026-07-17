34aefd1a939797fb4ca0b8b472cfc75eed604d61
les29rq <leo.strobel@uni-wuerzburg.de>
2026-07-17 12:49:03 +0200

    Remove unused import

--- src/main/kotlin/de/uniwuerzburg/omosim/core/ModeChoiceGTFS.kt
+++ src/main/kotlin/de/uniwuerzburg/omosim/core/ModeChoiceGTFS.kt
@@ -4,7 +4,7 @@ import com.graphhopper.GraphHopper
 import com.graphhopper.gtfs.PtRouter
 import de.uniwuerzburg.omosim.core.models.*
 import de.uniwuerzburg.omosim.io.ParameterReader
-import de.uniwuerzburg.omosim.routing.*
+import de.uniwuerzburg.omosim.routing.Route
 import de.uniwuerzburg.omosim.utils.ProgressBar
 import de.uniwuerzburg.omosim.utils.createCumDist
 import de.uniwuerzburg.omosim.utils.sampleCumDist
@@ -14,7 +14,6 @@ import kotlinx.coroutines.runBlocking
 import java.time.LocalDate
 import java.time.LocalTime
 import java.util.*
-import java.util.concurrent.atomic.AtomicInteger
 import kotlin.math.abs
 import kotlin.math.exp
 import kotlin.time.TimeSource
