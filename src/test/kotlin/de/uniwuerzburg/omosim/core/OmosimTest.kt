8191f05fc6cc33b96869ecaa43594670298a8a46
les29rq <leo.strobel@uni-wuerzburg.de>
2026-08-14 15:36:37 +0200

    Tests: run weekdays, run seed, mode choice car only

--- src/test/kotlin/de/uniwuerzburg/omosim/core/OmosimTest.kt
+++ src/test/kotlin/de/uniwuerzburg/omosim/core/OmosimTest.kt
@@ -3,6 +3,11 @@ package de.uniwuerzburg.omosim.core
 import com.google.common.jimfs.Configuration
 import com.google.common.jimfs.Jimfs
 import de.uniwuerzburg.omosim.core.models.MapDataSource
+import de.uniwuerzburg.omosim.core.models.Mode
+import de.uniwuerzburg.omosim.core.models.ModeChoiceOption
+import de.uniwuerzburg.omosim.core.models.Weekday
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 import org.locationtech.jts.geom.GeometryFactory
 import java.io.File
@@ -10,7 +15,7 @@ import java.io.File
 class OmosimTest {
     val areaFile = File(Omosim::class.java.classLoader.getResource("test_area.geojson")!!.file)
     val osmFile  = File(Omosim::class.java.classLoader.getResource("test.osm.pbf")!!.file)
-    val omosim   = Omosim(areaFile, osmFile)
+    val omosim   = Omosim(areaFile, osmFile, cache = false)
     val geometryFactory = GeometryFactory()
 
     @Test
@@ -76,5 +81,43 @@ class OmosimTest {
 
         assert(buildings.sumOf { it.osmProperties.number_place_of_worship }.toInt() == 1)
         assert(buildings.size == 5)
+    }
+
+    @Test
+    fun runTestDays() {
+        val agents = omosim.run(1, Weekday.TU, 3, verbose = false)
+        val actualDays = agents.first().mobilityDemand.map { it.dayType }
+        val expectedDays = listOf(Weekday.TU, Weekday.WE, Weekday.TH)
+
+        assertEquals(expectedDays, actualDays)
+    }
+
+    @Test
+    fun runTestSeed() {
+        omosim.mainRng.setSeed(123) // Reset rng
+        val agents1 = omosim.run(100, verbose = false)
+
+        omosim.mainRng.setSeed(123) // Reset rng
+        val agents2 = omosim.run(100, verbose = false)
+
+        assertEquals(agents1, agents2)
+    }
+
+    @Test
+    fun doModeChoiceTestCarOnly() {
+        val agents = omosim.run(100, verbose = false)
+        omosim.doModeChoice(agents, ModeChoiceOption.CAR_ONLY, withPath = false, verbose = false)
+
+        val allCar = agents.
+            flatMap { agent ->
+                agent.mobilityDemand.flatMap {
+                    diary -> diary.trips.map {
+                        trip -> trip.mode
+                    }
+                }
+            }
+            .all { it == Mode.CAR_DRIVER }
+
+        assertTrue(allCar)
     }
 }
